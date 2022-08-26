1e7a13432ce186ed673e3cafa73ee6c0d34f6f24
JorgeBanon <103133562+JorgeBanon@users.noreply.github.com>
2022-08-26 12:49:34 +0200

    Add files via upload

---
+++ sintaxis.js
@@ -0,0 +1,18 @@
+const libro = {
+    titulo: "Naufragios del cabo de Palos",
+    autor: "Jorge Bañón",
+    fecha: 2021,
+    enlace: "https://www.casadellibro.com/libro-naufragios-del-cabo-de-palos/9788412415865/12731265"
+}
+
+const datos = [
+    "Jorge",
+    62,
+    true,
+    Date("1960/08/11"),
+    libro
+];
+
+console.log(datos);
+
+
